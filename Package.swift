b865a5f1f5ffbbbd994ef859b5d3cb954a504350
Devi Prasad <devi.prasad@tempus.com>
2023-04-27 17:48:42 -0500

    fix: until issue is merged this is backup

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/supabase-community/gotrue-swift", from: "1.0.0"),
+    .package(url: "https://github.com/devi-prsd/gotrue-swift", branch: "main"),
     .package(url: "https://github.com/supabase-community/storage-swift.git", from: "0.1.1"),
     .package(url: "https://github.com/supabase-community/realtime-swift.git", from: "0.0.2"),
     .package(url: "https://github.com/supabase-community/postgrest-swift", from: "1.0.0"),
